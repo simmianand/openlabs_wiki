a86e423f9889ca268303eaa1a9f290d6cc098c04
tarunbhardwaj <mailme@tarunbhardwaj.com>
2013-02-25 17:43:01 +0530

    manage wiki done

--- openlabs_wiki/openlabs_wiki/urls.py
+++ openlabs_wiki/openlabs_wiki/urls.py
@@ -1,23 +1,26 @@
 from django.conf.urls import patterns, include, url
-from wiki.views import *
+from wiki.views import fwd_home, create_wiki, save_wiki, update_wiki, \
+    delete_wiki, history_wiki, history_show_wiki, compare_wiki,\
+    list_wiki, show_wiki, login
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', create_wiki),
-    url(r'^create_wiki$', create_wiki),
-    url(r'^save_wiki$',save_wiki),
-    url(r'^show_wiki$',show_wiki),
-    url(r'^update_wiki$',update_wiki),
-    # url(r'^openlabs_wiki/', include('openlabs_wiki.foo.urls')),
+urlpatterns = patterns(
+    '',
+    url(r'^$', fwd_home),
+    url(r'^wiki/$', fwd_home),
+    url(r'^wiki/_create$', create_wiki),
+    url(r'^wiki/_save$', save_wiki),
+    url(r'^wiki/login/$', login),
+    url(r'^wiki/(.*)/_update$', update_wiki),
+    url(r'^wiki/(.*)/_delete$', delete_wiki),
+    url(r'^wiki/(.*)/_history$', history_wiki),
+    url(r'^wiki/(.*)/_history/(\d+)/$', history_show_wiki),
+    url(r'^wiki/(.*)/_compare$', compare_wiki),
+    url(r'^wiki/_pages$', list_wiki),
+    url(r'^wiki/(.*)/$', show_wiki),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^/', include(openlabs_wiki.wiki.views.home_page)),
 )
--- openlabs_wiki/wiki/views.py
+++ openlabs_wiki/wiki/views.py
@@ -5,49 +5,147 @@
     :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
     :license: BSD, see LICENSE for more details.
 """
-import datetime
+from datetime import datetime
+
+import markdown
+import difflib
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
+from django.db import connection, transaction
 
-from wiki.models import *
+from wiki.models import wiki, wiki_count
+
+
+def fwd_home(request):
+    """Forward Url to Home-wiki.
+
+    :param request: No Use.
+    :return: Redirecting Url.
+    """
+    return HttpResponseRedirect('/wiki/home')
+
+
+def login(request):
+    """Forward Url to Home-wiki.
+
+    :param request: No Use.
+    :return: Redirecting Url.
+    """
+    return render_to_response('login.html')
 
 
 def create_wiki(request):
+    """Show create-wiki.html page
+
+    :param request: No Use.
+    :return: Render create-wiki.html template
+    """
     csrf_obj = {}
     csrf_obj.update(csrf(request))
     return render_to_response("create-wiki.html", csrf_obj)
 
+
 def get_wiki_id():
-    lastcount = wiki_count.objects.get(id=1)
-    lastcount.lastid=lastcount.lastid+1
-    lastcount.save()
+    """Fetch new wiki id.
+
+    :return: Wiki id for new wiki.
+    """
+    try:
+        lastcount = wiki_count.objects.get(id=1)
+        lastcount.lastid = lastcount.lastid+1
+        lastcount.save()
+    except:
+        lastcount = wiki_count(lastid=1, id=1)
+        lastcount.save()
     return lastcount.lastid
 
 
-def show_wiki(request):
-    lastcount = wiki_count.objects.get(id=1)
-    lastcount.lastid=lastcount.lastid+1
-    lastcount.save()
-    return HttpResponse(lastcount.lastid)
+def list_wiki(request):
+    """List all wiki pages.
 
-def update_wiki(request):
+    :param request: No Use.
+    :return: Rendered html with list of all wiki pages.
+    """
+    cursor = connection.cursor()
+    cursor.execute("SELECT DISTINCT(wiki_id), title FROM wiki_wiki \
+        ORDER BY title ASC")
+    values = {
+        "wiki_list": cursor.fetchall(),
+    }
+    #return HttpResponse(show_wiki)
+    return render_to_response('list-wiki.html', values)
+
+
+def show_wiki(request, title):
+    """Display wiki content as HTML
+
+    :param title: To Map to wiki.
+    :return: Render show-wiki.html with wiki content.
+    """
+    if not title:
+        title = 'home'
+    show_wiki = wiki.objects.raw("SELECT * FROM wiki_wiki WHERE \
+        UPPER(title) = UPPER('"+title+"') ORDER BY pub_date DESC LIMIT 1")[0]
+    content = markdown.markdown(show_wiki.content, safe_mode=True)
+    values = {
+        "wiki_title": show_wiki.title,
+        "wiki_content": content,
+        "time": show_wiki.pub_date,
+        "user": 'openlabs',
+    }
+    return render_to_response('show-wiki.html', values)
+
+
+def update_wiki(request, title):
+    """Insert New Row in table against previous wiki_id
+
+    :param title: To Map to wiki.
+    :return: Render update-wiki.html.
+    """
     try:
-        last_wiki = wiki.objects.raw("SELECT * FROM wiki_wiki WHERE wiki_id = %s ORDER BY pub_date DESC LIMIT 1" % request.GET['wiki_id'])[0]
+        last_wiki = wiki.objects.raw("SELECT * FROM wiki_wiki WHERE \
+            UPPER(title) = UPPER('"+title+"') ORDER BY pub_date \
+            DESC LIMIT 1")[0]
         values = {
             "wiki_content": last_wiki.content,
             "wiki_title": last_wiki.title,
             "wiki_type": last_wiki.wiki_type,
             "wiki_id": last_wiki.wiki_id,
-            }
+        }
         values.update(csrf(request))
         return render_to_response('update-wiki.html', values)
     except:
-        return HttpResponse("error")
-        #return HttpResponseRedirect('create_wiki')
+        return HttpResponseRedirect('create_wiki')
+
+
+def update_wiki_check(request):
+    """Check if update is allowed or not.
+
+    :return: 1 if allowed allowed.
+             2 if No Change.
+    """
+    try:
+        last_wiki = wiki.objects.raw("SELECT * FROM wiki_wiki WHERE \
+            wiki_id = '"+request.POST['wiki_id']+"' ORDER BY pub_date \
+            DESC LIMIT 1")[0]
+        if (last_wiki.title == request.POST['title'] and
+                last_wiki.content == request.POST['content']):
+            return 2
+
+        if last_wiki.title == request.POST['title']:
+            return 1
+    except:
+        return 1
+
 
 def save_wiki(request):
+    """Save wiki to database.
+
+    :param request: Read user input from ajax
+    :return: json message with error, message [, element] fields.
+    """
     if request.method is not 'POST':
         try:
             title = request.POST['title']
@@ -81,18 +179,23 @@ def save_wiki(request):
                 "element": "visibility"\
             }')
 
-        if not request.POST['wiki_id']:
+        if 'wiki_id' not in request.POST:
             wiki_id = get_wiki_id()
         else:
-            wiki_id=request.POST['wiki_id']
-
-
+            wiki_id = request.POST['wiki_id']
+            status = update_wiki_check(request)
+            if status == 2:
+                return HttpResponse('{\
+                    "error": "false",\
+                    "message": "Wiki Saved",\
+                    "element": "none"\
+                }')
         wiki_obj = wiki(
             wiki_id=wiki_id,
             title=title,
             content=content,
             comment=comment,
-            pub_date=datetime.datetime.now(),
+            pub_date=datetime.now(),
             wiki_type=wiki_type,
         )
         wiki_obj.save()
@@ -103,3 +206,96 @@ def save_wiki(request):
             }')
     else:
         return HttpResponse('Error')
+
+
+def delete_wiki(request, title):
+    """Delete wiki page with all previous commits.
+
+    :param request: read wiki_id
+    :param title: no use.
+    :return: json string with error, message
+    """
+    if 'wiki_id' in request.GET and title.lower() != "home":
+        try:
+            cursor = connection.cursor()
+            cursor.execute("DELETE FROM wiki_wiki WHERE \
+                wiki_id = "+request.GET['wiki_id'])
+            transaction.commit_unless_managed()
+            return HttpResponse('{\
+                "error": "false",\
+                "message": "deleted"\
+                }')
+        except:
+            return HttpResponse('{\
+                "error": "true",\
+                "message": "Failed"\
+                }')
+    else:
+        return HttpResponse('{\
+                "error": "true",\
+                "message": "Not Allowed"\
+                }')
+
+
+def history_wiki(request, title):
+    """show list history page of wiki
+
+    :param request: no use.
+    :return: render history-wiki.html if found wiki, else print error.
+    """
+    try:
+        wiki_history = wiki.objects.raw("SELECT * FROM wiki_wiki WHERE \
+            UPPER(title) = UPPER('"+title+"') ORDER BY pub_date DESC")
+        values = {
+            'wiki_history_list': wiki_history,
+            'title': title,
+        }
+        values.update(csrf(request))
+        return render_to_response('history-wiki.html', values)
+    except:
+        return HttpResponse('error')
+
+
+def history_show_wiki(request, title, history_id):
+    """Display history for wiki.
+
+    :param title: Title of wiki.
+    :param history_id: ID of history.
+    :return: Show previous wiki version if wiki exist. Else 'error'
+    """
+    try:
+        show_wiki = wiki.objects.raw("SELECT * FROM wiki_wiki WHERE \
+        id = "+history_id)[0]
+        content = markdown.markdown(show_wiki.content, safe_mode=True)
+        time = show_wiki.pub_date
+        values = {
+            "wiki_title": show_wiki.title,
+            "wiki_content": content,
+            "time": time,
+            "user": 'openlabs',
+        }
+
+        return render_to_response('show-history-wiki.html', values)
+    except:
+        return HttpResponse('error')
+
+
+def compare_wiki(request, title):
+    """compare wikis
+
+    :param request: get wiki_ids from user
+    :return: render compare-wiki.html if wiki exist else error.
+    """
+    if 'wiki1' in request.POST:
+        diff = difflib.HtmlDiff()
+        diff = difflib.Differ()
+        wiki1 = wiki.objects.get(id=request.POST['wiki1'])
+        wiki2 = wiki.objects.get(id=request.POST['wiki2'])
+        values = {
+            'content': list(diff.compare(wiki1.content.split('\n'),
+            wiki2.content.split('\n'))),
+            'title': title,
+        }
+        return render_to_response('compare-wiki.html', values)
+    else:
+        return HttpResponse('error')
